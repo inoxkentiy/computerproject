e851b1e355006e95f10f15ee7e4c15e0c869c277
kesh <92874131+inoxkentiy@users.noreply.github.com>
2021-11-10 00:19:47 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,11 @@
-let buttonBlock = $("#buttons_block")
-let outputBlock = $("#output_block")
-let buttonsArray = ['C', '√', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, 0, '.', '=']
-buttonsArray.map(item => {
-    buttonBlock.append(`
-    <div class="button_item">
-    ${item}
-    </div>`)
-})
 
-let buttons = document.querySelectorAll(".button_item")
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (event) => {
-        let buttonValue = event.currentTarget.innerText;
-        if (buttonValue === '=') {
-            let outputBlockBalue = outputBlock.text()
-            let result = eval(outputBlockBalue);
-            outputBlock.empty()
-            outputBlock.append(result)
-            listDiv = $("#list")
-            listDiv.append(`${outputBlockBalue}=${result}`)
-        } else if (buttonValue === '√') {
-            let outputBlockBalue = outputBlock.text()
-            let result = Math.sqrt(outputBlockBalue);
-            outputBlock.empty()
-            outputBlock.append(result)
-
-            listDiv = $("#list")
-            listDiv.append(`√${outputBlockBalue}=${result}`)
-        }
-        else {
-            outputBlock.append(buttonValue)
-        }
-    })
-    buttons[0].addEventListener('click', (event) => {
-        outputBlock.empty()
-    })
-}
+document.addEventListener('DOMContentLoaded', function () {
+    let input = document.getElementById('selprocessor');
+    if (localStorage['selprocessor']) {
+        input.value = localStorage['selprocessor'];
+    }
+    document.getElementById("submit").addEventListener('click', function () {
+        localStorage['selprocessor'] = this.value;
+    }) 
+});
+console.log(selprocessor)
